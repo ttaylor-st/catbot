2011202df00dcef8bf8025cfbd350a6782086c9a
Taylor <ttaylor-st@murena.io>
2024-07-08 21:56:25 +0100

    feat: implement basic bot

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+import * as fs from "node:fs";
+import { DiscuitClient } from "discuit-ts";
+import { logger } from "./Logger";
+import type { Config } from "./types";
+
+const config: Config = JSON.parse(fs.readFileSync("./config.json", "utf-8"));
+
+const client = new DiscuitClient({
+	baseURL: "https://discuit.net/api/",
+});
+
+await client.initialize();
+const user = await client.login(config.username, config.password);
+logger.info(`Successfully logged in as ${user.username}`);
