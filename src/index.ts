be42a88ae858fa697891d0af6f9939a10611001f
Taylor <ttaylor-st@murena.io>
2024-07-09 17:25:32 +0100

    feat: watch for new posts and comments

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
 import * as fs from "node:fs";
-import { DiscuitClient } from "discuit-ts";
+import { type Comment, DiscuitClient } from "discuit-ts";
 import { logger } from "./Logger";
+import { Watcher } from "./Watcher";
 import type { Config } from "./types";
 
 const config: Config = JSON.parse(fs.readFileSync("./config.json", "utf-8"));
 
 const client = new DiscuitClient({
-	baseURL: "https://discuit.net/api/",
+	baseURL: "https://discuit.ttaylor.run.place/api/",
 });
 
-await client.initialize();
 const user = await client.login(config.username, config.password);
 logger.info(`Successfully logged in as ${user.username}`);
+
+const watcher = new Watcher(client);
+watcher.startWatching();
+watcher.on("newPost", (post) => {
+	logger.info(`New post: ${post.title}`);
+});
+
+watcher.on("newComment", (comment: Comment) => {
+	logger.info(`New comment: ${comment.body}`);
+});
