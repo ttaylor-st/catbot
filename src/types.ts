5004fdaa7eff9f797f1e5b044baad62429a163f4
Taylor <ttaylor-st@murena.io>
2024-07-11 02:55:37 +0100

    feat: add Command type

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,18 @@
+import type { Comment } from "@ttaylor-st/discuit-ts";
+
 export type Config = {
 	username: string;
 	password: string;
 	baseUrl: string;
+};
+
+export type Command = {
+	/** The name of the command. */
+	fullName: string;
+	/** The aliases of the command. */
+	aliases: string[];
+	/** The communities that this command should be executed in. Empty array means all communities. */
+	communities: string[] | [];
+	/** The function to execute when this command is called. */
+	execute: (comment: Comment) => Promise<void>;
 };
